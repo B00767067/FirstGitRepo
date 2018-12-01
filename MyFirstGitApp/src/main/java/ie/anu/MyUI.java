93094b2463cd0f21da78b792ec97cea1e6a6aa2e
Anu Garg <Garg-A1@ulster.ac.uk>
2018-12-01 15:36:33 +0000

    Adding the grid

--- MyFirstGitApp/src/main/java/ie/anu/MyUI.java
+++ MyFirstGitApp/src/main/java/ie/anu/MyUI.java
@@ -2,13 +2,17 @@ package ie.anu;
 
 import javax.servlet.annotation.WebServlet;
 
+//import java.sql.*;
 import java.sql.*;
+import java.util.*;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.shared.ui.grid.*;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
@@ -29,11 +33,12 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final HorizontalLayout layout = new HorizontalLayout();
+        final VerticalLayout layout = new VerticalLayout();
 
         //first step to connect with database
         Connection connection = null;
         
+        //String to connect to database and get info from database resource in azzure under connectionn string 
         String connectionString = "jdbc:sqlserver://anuclassdb.database.windows.net:1433;" + 
 			  "database= classdb;" + 
 			  "user=anu@anuclassdb;" + 
@@ -42,7 +47,8 @@ public class MyUI extends UI {
 			  "trustServerCertificate=false;" + 
 			  "hostNameInCertificate=*.database.windows.net;" +
               "loginTimeout=30;";
-           
+
+              //step 3 add try and catch statement so as to know that you are connected to database         
               
 
               try 
@@ -50,13 +56,38 @@ public class MyUI extends UI {
                   // Connect with JDBC driver to a database
                   connection = DriverManager.getConnection(connectionString);
                   // Add a label to the web app with the message and name of the database we connected to 
-                  layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
+                  //layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
+                  ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM customerTable;");
+                  // Convert the resultset that comes back into a List - we need a Java class to represent the data (Customer.java in this case)
+                  List<Customer> customers = new ArrayList<Customer>();
+                  // While there are more records in the resultset
+                  while(rs.next())
+                  {   
+                      // Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields, note how I skip the id)
+                      customers.add(new Customer(rs.getString("first_name"), 
+                                  rs.getString("last_name"), 
+                                  rs.getBoolean("paid"), 
+                                  rs.getDouble("amount")));
+                  }
+                  // Add my component, grid is templated with Customer
+Grid <Customer> myGrid = new Grid<>();
+// Set the items (List)
+myGrid.setItems(customers);
+// Configure the order and the caption of the grid
+myGrid.addColumn(Customer::getFirst_name).setCaption("Name");
+myGrid.addColumn(Customer::getLast_name).setCaption("Surname");
+myGrid.addColumn(Customer::getAmount).setCaption("Total Amount");
+myGrid.addColumn(Customer::isPaid).setCaption("Paid");
+
+// Add the grid to the list
+layout.addComponent(myGrid);
               } 
               catch (Exception e) 
               {
                   // This will show an error message if something went wrong
                   layout.addComponent(new Label(e.getMessage()));
               }
+              
               setContent(layout);
     }
 
